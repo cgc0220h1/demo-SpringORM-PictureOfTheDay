e51ce0dc2eff3b4cad2bf1d9d88eb606efa200ae
Duc <vanduc2514@gmail.com>
2020-06-10 00:39:03 +0700

    Refactor

--- src/main/java/controllers/ImageController.java
+++ src/main/java/controllers/ImageController.java
@@ -1,8 +1,6 @@
 package controllers;
 
 import model.Comment;
-import model.Picture;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
